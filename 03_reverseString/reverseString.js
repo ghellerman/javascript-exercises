c0199d1b8197b4b12a9b20a32e19be20e63ecc24
ghellerman <ghellerman@live.ca>
2022-06-10 14:08:33 -0400

    exercise 1, 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let splitString = inputString.split("");
+    let reverseArray = splitString.reverse();
+    let outputString = reverseArray.join("");
+    return outputString;
 };
 
 // Do not edit below this line
