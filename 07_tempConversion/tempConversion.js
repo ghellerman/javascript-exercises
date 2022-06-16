0a7a00295075f24218fd67d15ee7cea7e7e19102
ghellerman <ghellerman@live.ca>
2022-06-15 21:08:30 -0400

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC;
+  tempC = (tempF - 32) * (5 / 9);
+  let roundedTemp = Math.round(tempC * 10) / 10;
+  return roundedTemp
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF;
+  tempF = tempC * (9/5) + 32;
+  let roundedTemp = Math.round(tempF * 10) / 10;
+  return roundedTemp
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+ return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
 const power = function() {
-	
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let result = 1;
+	for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
